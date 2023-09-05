54d29f0ba2573b12864fbbd855924b77f043067c
Shivam Patle <144086437+shivampatle1@users.noreply.github.com>
2023-09-05 17:06:01 +0000

    convertion

---
+++ 01_basics/03_convertionopraration.js
@@ -0,0 +1,30 @@
+let score="33abc"
+
+console.log(typeof score);
+
+let valueINumber=Number(score)
+console.log(typeof valueINumber);
+console.log(valueINumber);
+
+//"33"=>33
+//"33abc"=>Nan (not an number)
+//true=>1; false=>0
+
+let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1=>true; 0=>false
+//""=>false
+
+//"shivam=>true
+
+
+
+let someNumber=35
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
