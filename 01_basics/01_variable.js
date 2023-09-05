5508df6440f2e70da63da6a2fa1d249e633814b9
Shivam Patle <144086437+shivampatle1@users.noreply.github.com>
2023-09-05 07:08:50 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accontId=12345
+let accontEmail="shivam@google.com"
+var accountPassword="123456"
+accountCity="Jaipur"
+let accountState;
+
+
+//const cant rename
+
+
+/*
+prefer not to use var because of issue in block space and functional scope but in old java course uses var 
+*/
+
+accontEmail="patle@google.com"
+accountPassword="969142"
+accountCity="mumbai"
+
+console.table([ accontId,accontEmail, accountPassword, accountCity , accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat All JS code as newer version
+
+// alert(3+3) we are using nodejs , not browser
+
+console.log(3+3);//code rediability should be high
+
+console.log("Shivam");
+
+
+let name="Shivam"
+let age=17
+let isLoggedIn= false
+let tempreture=null
+
+// number=>2 to power 53 
+//bigInt
+//string =>""
+//boolean =>true/false
+//null => stand alone value
+//undefined =>
+//symbol => unique
+
+//object
+
+//console.log(typeof null); use for check type
+
+
+
