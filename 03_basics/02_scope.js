8e8a11e998bdede95f39d82ea0ddb17cef350ad8
Shivam Patle <144086437+shivampatle1@users.noreply.github.com>
2023-09-29 14:50:25 +0000

    scope level and mini hosting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,12 +1,61 @@
 //var c=300
-let a=300
+//let a=300
 if (true) {
     let a=10
     const b=20
-   // var c=30  
-   console.log("inner:", a);
+   // var c=30
+   //console.log("inner:", a);
 }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username ="Shivam"
+
+    function two() {
+        const website = "youtube"
+        //console.log(username);
+        
+    }
+
+    //console.log(website);
+
+    two()
+}
+
+one()  
+
+
+//bda chote se le skta hai par chote se bda nhi le skta hai
+
+if (true) {
+    const username="Shivam"
+    if (username==="Shivam") {
+        const website="  - youtube"
+        //console.log(username+website);
+    }
+
+    //console.log(website);
+    
+}
+
+//console.log(username);
+
+//*********************** intresting************************** */
+
+
+console.log(addone(5));
+function addone(num){
+  return num+1
+}
+
+
+
+addtwo(5)
+const addtwo = function(num){
+    return num +2
+}
+
+//addtwo(5)
