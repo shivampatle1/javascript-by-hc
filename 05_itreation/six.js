76ce8bc03e2d3d2ccd929aabe985b2a731d22276
Shivam Patle <144086437+shivampatle1@users.noreply.github.com>
2023-11-24 11:21:03 +0000

    js

---
+++ 05_itreation/six.js
@@ -0,0 +1,10 @@
+//const coding=["js","ruby", "java", "python","cpp"]
+
+//const values =coding.forEach( (item) =>{
+//    console.log(item);
+//    return item
+//}
+//)
+
+//console.log(values); 
+
